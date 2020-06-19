de8aaa0c3f5cc38f5efa49c13a7f79fcc58d3809
Samuel Colvin <s@muelcolvin.com>
2020-06-19 18:36:27 +0100

    improve code visibility

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const path = require('path')
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   target: 'webworker',
   entry: './src/index.js',
   mode: 'production',
-  // devtool: "cheap-module-source-map",
+  optimization: {
+    minimize: false,
+    namedModules: true,
+    namedChunks: false,  // breaks if true
+    mangleWasmImports: false,
+  },
   plugins: [
+    new CleanWebpackPlugin(),
     new WasmPackPlugin({
       crateDirectory: path.resolve(__dirname, '.'),
       outDir: 'src/pkg',
