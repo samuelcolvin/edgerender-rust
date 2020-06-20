0c1670f556f2a89ca06a5a5c8892e035cdf969c8
Samuel Colvin <s@muelcolvin.com>
2020-06-20 19:45:19 +0100

    using templates from s3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import edgerender from './src/index'
 
-const CONFIG_URL = 'https://edgerender.s3-eu-west-1.amazonaws.com/edgerender.toml'
+const CONFIG_URL = 'https://edgerender.s3-eu-west-1.amazonaws.com/edgerender.yaml'
 addEventListener('fetch', e => edgerender(e, CONFIG_URL))
--- src/index.js
+++ src/index.js
@@ -1,69 +1,29 @@
-import YAML from 'yaml'
+import {load_config, load_templates_s3} from './config'
 
-export default function (event, config_url) {
+export default function(event, config_url) {
   return event.respondWith(wrap_error(event.request, config_url))
 }
 
 async function wrap_error(request, config_url) {
-  console.log('request:', request)
-  console.log('config_url:', config_url)
+  console.log(`config_url: ${config_url} request:`, request)
   try {
     return await handle(request, config_url)
   } catch (e) {
     console.error(`error handling request to ${request.url}:`, e)
     // TODO log error
-    return new Response(`Error handling request: ${e}`, {
+    return new Response(`Error handling request:\n\n  ${e.message}`, {
       status: 500,
       headers: {'content-type': 'text/plain'},
     })
   }
 }
 
-const some_yaml = `
-foo: "this is foo"
-bar: 123
-yaml:
-  - A complete JavaScript implementation
-  - https://www.npmjs.com/package/yaml
-`
-
-const templates = [
-  {
-    name: 'base.jinja',
-    content: `
-<!doctype html>
-<html lang="en">
-  <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>{{ title }}</title>
-  </head>
-  <body>
-    {% block main %}
-      <b>(main block)</b>
-    {% endblock %}
-  </body>
-</html>
-`,
-  },
-  {
-    name: 'main.jinja',
-    content: `
-{% extends 'base.jinja' %}
-
-{% block main %}
-  <p>{{ date|date(format="%Y-%m-%d %A %H:%M:%S") }}</p>
-  <ul>
-    {% for name, item in things %}
-      <li><b>{{ name }}:</b> {{ item }}</li>
-    {% endfor %}
-  </ul>
-{% endblock %}
-`,
-  },
-]
-
 async function handle(request, config_url) {
+  const config = await load_config(config_url)
+  console.log('config:', config)
+  const templates = await load_templates_s3(config)
+  console.log('templates:', templates)
+
   const {create_env} = await import('./edgerender-pkg')
   let env
   try {
@@ -71,26 +31,27 @@ async function handle(request, config_url) {
   } catch (e) {
     if (e instanceof SyntaxError) {
       // this is an invalid templates
+      console.log('templates:', templates)
       console.warn('invalid template:', e)
       return new Response(`Invalid Template\n\n${e.message}`, {status: 502})
     } else {
+      console.log('templates:', templates)
       console.error('error creating template environment:', e)
       return new Response(`Error Creating Template Environment\n\n${e.message}`, {status: 500})
     }
   }
-  console.log('env:', env)
 
   const context = {
     title: 'This is working!',
     date: new Date(),
-    things: {
+    items: {
       Foo: 'Bar',
       Apple: 'Pie',
     },
   }
-  const config = YAML.parse(some_yaml)
-  console.log('config:', config)
-  Object.assign(context, config)
+  if (config.context) {
+    Object.assign(context, config.context)
+  }
 
   let html
   try {
