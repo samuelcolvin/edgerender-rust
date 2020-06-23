9fb1f4f14395c9dd3bd472eed5b700920bf95d24
Samuel Colvin <s@muelcolvin.com>
2020-06-23 12:19:42 +0100

    size tweaks

--- index.js
+++
@@ -1,5 +0,0 @@
-import edgerender from './src/index'
-
-const CONFIG_URL = 'https://edgerender.s3-eu-west-1.amazonaws.com/edgerender.yaml'
-const CACHE_FLUSH_KEY = 'testing'
-addEventListener('fetch', e => edgerender(e, CONFIG_URL, CACHE_FLUSH_KEY))
